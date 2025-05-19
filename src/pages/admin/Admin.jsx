bedbdb0883560cee547b1ac60480d3a120ee944a
Tobias Alves <tobiasjose12@gmail.com>
2025-05-19 19:20:29 -0300

    Update Admin.jsx

--- src/pages/admin/Admin.jsx
+++ src/pages/admin/Admin.jsx
@@ -2,14 +2,7 @@ import { Link } from "react-router-dom";
 export default function AdminHome() {
   return (
     <main>
-      <header>
-        <nav style={{ display: "flex", gap: "2rem" }}>
-        <Link to="/">Início</Link>
-          <Link to="/products">Produtos</Link>
-          <Link to="/cart">Carrinho</Link>
-          <Link to="/admin">Administração</Link>
-        </nav>
-      </header>
+      <Header />
       <h1>Área Administrativa</h1>
       <p>
         Esta é a área de administração da loja. Aqui você poderá adicionar
