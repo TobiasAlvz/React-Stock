6b14e4436bd68e55f9d9b2033c1afec2c6ef4fd7
Tobias Alves <tobiasjose12@gmail.com>
2025-05-19 19:06:40 -0300

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,8 +5,10 @@ export default function Home() {
     <section>
       <header>
         <nav style={{ display: "flex", gap: "2rem" }}>
+          <Link to="/">Início</Link>
           <Link to="/products">Produtos</Link>
           <Link to="/cart">Carrinho</Link>
+          <Link to="/admin">Administração</Link>
         </nav>
       </header>
       <h2>Página inicial!</h2>
