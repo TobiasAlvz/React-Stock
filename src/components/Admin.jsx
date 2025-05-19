4e0ecafda6d235f923669f0a7ab3087ab276e522
Tobias Alves <tobiasjose12@gmail.com>
2025-05-19 19:03:54 -0300

    Update Admin.jsx

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -1,8 +1,18 @@
+import { Link } from "react-router-dom";
 export default function AdminHome() {
   return (
     <main>
+      <header>
+        <nav style={{ display: "flex", gap: "2rem" }}>
+          <Link to="/products">Produtos</Link>
+          <Link to="/cart">Carrinho</Link>
+        </nav>
+      </header>
       <h1>Área Administrativa</h1>
-      <p>Esta é a área de administração da loja. Aqui você poderá adicionar produtos e ver os pedidos.</p>
+      <p>
+        Esta é a área de administração da loja. Aqui você poderá adicionar
+        produtos e ver os pedidos.
+      </p>
     </main>
-  )
+  );
 }
