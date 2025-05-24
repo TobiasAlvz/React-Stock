a025d47e1dacd27ae6a93f8481a8ec0c795ae039
Tobias Alves <tobiasjose12@gmail.com>
2025-05-24 19:06:32 -0300

    Update Product.jsx

---
+++ src/pages/Product.jsx
@@ -0,0 +1,20 @@
+import { Link, useParams } from "react-router-dom";
+import products from "../database.json";
+
+export default function Product() {
+  const { productId } = useParams();
+
+  const product = products.find((p) => p.id === +productId);
+
+  return (
+    <section>
+      <Link to="/products">
+        <button>Voltar</button>
+      </Link>
+      <h2>{product.name}</h2>
+      <p>{product.description}</p>
+      <p>R$ {product.price}</p>
+      <button>Comprar</button>
+    </section>
+  );
+}
