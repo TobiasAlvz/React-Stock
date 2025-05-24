95828d6544377fc3be57ce68e62e740b651b2528
Tobias Alves <tobiasjose12@gmail.com>
2025-05-24 19:06:39 -0300

    Update router.jsx

--- src/router.jsx
+++ src/router.jsx
@@ -3,7 +3,10 @@ import RootLayout from "./pages/Layout";
 import Admin from "./pages/admin/Admin";
 import Home from "./pages/Home";
 import Products from "./pages/Products";
+import Product from "./pages/Product";
 import Cart from "./pages/Cart";
+import { loadProduct } from "./loaders/products";
+import ProductBoundary from "./error-boundaries/ProductBoundary";
 
 const router = createBrowserRouter([
   {
@@ -21,6 +24,8 @@ const router = createBrowserRouter([
       {
         path: "products/:productId",
         element: <Product />,
+        loader: loadProduct,
+        errorElement: <ProductBoundary />,
       },
       {
         path: "cart",
