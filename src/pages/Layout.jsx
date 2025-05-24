9daa74ca2ffe06b8aba4ab6b8e64815e3af7e953
Tobias Alves <tobiasjose12@gmail.com>
2025-05-24 18:19:36 -0300

    Update Layout.jsx

---
+++ src/pages/Layout.jsx
@@ -0,0 +1,15 @@
+
+import { Outlet, Link } from "react-router-dom";
+import Header from "./Header";
+
+export default function RootLayout() {
+  return (
+    <>
+      <Header />
+      <Outlet />
+      <footer>
+        <p>Feito com React Router</p>
+      </footer>
+    </>
+  );
+}
