c9bb9e29c9ec355aa105d5691215aaf8fffb0f0f
Tobias Alves <tobiasjose12@gmail.com>
2025-05-24 18:19:33 -0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,10 @@
 
-import Header from "./Header";
+
 
 export default function Home() {
   return (
     <section>
-      <Header />
+
       <h2>Página inicial!</h2>
       <p>Seja bem-vindo(a) à nossa loja.</p>
     </section>
