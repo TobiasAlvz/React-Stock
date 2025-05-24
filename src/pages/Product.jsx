41684acd2e1ecc6fa00be9de083bac9b222db182
Tobias Alves <tobiasjose12@gmail.com>
2025-05-24 19:22:20 -0300

    Update Product.jsx

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,10 +1,7 @@
-import { Link, useParams } from "react-router-dom";
-import products from "../database.json";
+import { Link, useLoaderData } from "react-router-dom";
 
 export default function Product() {
-  const { productId } = useParams();
-
-  const product = products.find((p) => p.id === +productId);
+  const product = useLoaderData();
 
   return (
     <section>
