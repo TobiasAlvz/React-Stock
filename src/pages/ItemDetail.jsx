2ce097917efe9d6684f3b9f568a424c12951e06b
Tobias Alves <tobiasjose12@gmail.com>
2025-05-25 21:23:25 -0300

    Update ItemDetail.jsx

--- src/pages/ItemDetail.jsx
+++ src/pages/ItemDetail.jsx
@@ -1,17 +1,20 @@
-import { Link, useLoaderData } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import { useEstoque } from "../contexts/EstoqueContext";
 
-export default function Product() {
-  const product = useLoaderData();
+export default function ItemDetail() {
+  const { items } = useEstoque();
+  const { id } = useParams();
+  const item = items.find(item => item.id === Number(id));
 
   return (
     <section>
-      <Link to="/products">
-        <button>Voltar</button>
-      </Link>
-      <h2>{product.name}</h2>
-      <p>{product.description}</p>
-      <p>R$ {product.price}</p>
-      <button>Comprar</button>
+      <Link to="/items">Voltar</Link>
+      <h2>{item.name}</h2>
+      <p><strong>Quantidade:</strong> {item.quantity}</p>
+      <p><strong>Preço:</strong> R$ {item.price.toFixed(2)}</p>
+      <p><strong>Categoria:</strong> {item.category}</p>
+      <p><strong>Descrição:</strong> {item.description}</p>
+      <Link to={`/items/${item.id}/edit`}>Editar</Link>
     </section>
   );
 }
