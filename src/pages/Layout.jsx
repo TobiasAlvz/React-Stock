28b714a4afc753b5d931c100454d50648b146c03
Tobias Alves <tobiasjose12@gmail.com>
2025-05-25 22:57:55 -0300

    Update Layout.jsx

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,15 +1,16 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import Footer from "./Footer";
+import styles from './Layout.module.css'; 
 
 export default function RootLayout() {
   return (
-    <>
+    <div className={styles.wrapper}>
       <Header />
-      <main>
+      <main className={styles.content}>
         <Outlet />
       </main>
       <Footer />
-    </>
+    </div>
   );
 }
