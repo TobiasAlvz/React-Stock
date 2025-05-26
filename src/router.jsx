28a69f7a94a7884af55a50403007234e82ef8a1a
Tobias Alves <tobiasjose12@gmail.com>
2025-05-25 21:50:53 -0300

    Update router.jsx

--- src/router.jsx
+++ src/router.jsx
@@ -1,11 +1,11 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import Layout from "./pages/Layout";
 import Dashboard from "./pages/Dashboard";
 import ItemsList from "./pages/ItemsList";
 import ItemDetail from "./pages/ItemDetail";
 import NewItem from "./pages/NewItem";
 import EditItem from "./pages/EditItem";
-import Cart from "./pages/admin/Cart";
+import Cart from "./pages/admin/Cart"; 
 
 const router = createBrowserRouter([
   {
@@ -17,7 +17,9 @@ const router = createBrowserRouter([
       { path: "items/:id", element: <ItemDetail /> },
       { path: "items/new", element: <NewItem /> },
       { path: "items/:id/edit", element: <EditItem /> },
-      { path: "admin/cart", element: <Cart /> },
+      { path: "admin/cart", element: <Cart /> }, 
     ],
   },
 ]);
+
+export default router;
