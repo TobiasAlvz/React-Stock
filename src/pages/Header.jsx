407d5bfd4aeb4e05d0c7b1335957f754cffda8eb
Tobias Alves <tobiasjose12@gmail.com>
2025-05-25 22:14:44 -0300

    Update Header.jsx

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,12 +1,35 @@
+import styles from "./Header.module.css";
 import { NavLink } from "react-router-dom";
 
 export default function Header() {
   return (
-    <header>
-      <nav>
-        <NavLink to="/">Dashboard</NavLink>
-        <NavLink to="/items">Itens</NavLink>
-        <NavLink to="/admin/cart">Carrinho</NavLink> {}
+    <header className={styles.navbar}>
+      <h1 className={styles.logo}>REACT STOCK</h1>
+      <nav className={styles.navLinks}>
+        <NavLink
+          to="/"
+          className={({ isActive }) =>
+            isActive ? `${styles.link} ${styles.active}` : styles.link
+          }
+        >
+          Dashboard
+        </NavLink>
+        <NavLink
+          to="/items"
+          className={({ isActive }) =>
+            isActive ? `${styles.link} ${styles.active}` : styles.link
+          }
+        >
+          Items
+        </NavLink>
+        <NavLink
+          to="/cart"
+          className={({ isActive }) =>
+            isActive ? `${styles.link} ${styles.active}` : styles.link
+          }
+        >
+          Carrinho
+        </NavLink>
       </nav>
     </header>
   );
