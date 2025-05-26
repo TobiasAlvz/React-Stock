9397f485b088fc72538aacaeb482f9a4a2b3941c
Tobias Alves <tobiasjose12@gmail.com>
2025-05-25 21:50:42 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// src/main.jsx
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import { EstoqueProvider } from './contexts/EstoqueContext' // Caminho correto!
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <EstoqueProvider> 
+      <App />
+    </EstoqueProvider>
+  </React.StrictMode>
 )
