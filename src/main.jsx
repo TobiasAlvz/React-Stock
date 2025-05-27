e0b079248f04eebb7a85e5ce1b2d1e1cdb370f94
Tobias Alves <tobiasjose12@gmail.com>
2025-05-26 23:32:40 -0300

    up

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { EstoqueProvider } from "./contexts/EstoqueContext";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <EstoqueProvider>
+      <App />
+    </EstoqueProvider>
+  </React.StrictMode>
+);
