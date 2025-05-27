5c0bc6294283d302a6e1ada3c3e8b91d9d2d3017
Tobias Alves <tobiasjose12@gmail.com>
2025-05-26 21:35:00 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import { EstoqueProvider } from './contexts/EstoqueContext' // Caminho correto!
+import { EstoqueProvider } from './contexts/EstoqueContext' 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
